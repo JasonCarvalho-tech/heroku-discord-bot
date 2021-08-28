167ba1bba3a280afb2210f2b64f353291ce0be90
Jason Carvalho <jml123carvalho@gmail.com>
2021-08-28 16:51:24 +0530

    testing if token is displayed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 console.log('this is working');
+console.log('token' + process.env.DISCORD_TOKEN);
 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
