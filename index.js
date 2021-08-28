e0b874365867ff9f66077facf1c981e9306cedc1
Jason Carvalho <jml123carvalho@gmail.com>
2021-08-28 16:24:59 +0530

    removed unnecessary console.log

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-console.log(process.env.DISCORD_TOKEN);
 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
